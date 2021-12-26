3f334dd6c2b5c761a9fb5fb3cc44b1fb2e67871c
Tris Emmy Wilson <tris@tris.fyi>
2021-12-25 21:09:12 -0600

    initial commit with pydoc

---
+++ README.md
@@ -0,0 +1,12 @@
+# Amethyst extensions
+
+This repository contains extensions for the Amethyst Gemini server.
+
+You can learn more about Amethyst:
+
+* [on its GitHub repository](https://github.com/an-empty-string/amethyst)
+* in Geminispace at gemini://gemini.tris.fyi/projects/amethyst/
+
+The extensions in this repository are:
+
+* **pydoc**: a Python documentation browser. No parameters.
---
+++ setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+from setuptools import find_packages, setup
+
+setup(
+    name="amethyst_extensions",
+    version="0.0.1",
+    description="Extensions for the Amethyst server",
+    author="Tris Emmy Wilson",
+    author_email="tris@tris.fyi",
+    packages=find_packages(),
+    entry_points={
+        "amethyst.resources": [
+            "pydoc = amethyst_ext.pydoc:PydocResource",
+        ],
+    },
+)
+
